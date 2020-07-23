f6f64d300a4d3ec85db4a0f2042dc7c2ee28215b
jaykop <jaykop.jy@gmail.com>
2020-07-23 12:20:32 -0700

    colored model

--- game/shader/model.fs
+++ game/shader/model.fs
@@ -7,7 +7,9 @@ uniform sampler2D texture_diffuse1;
 uniform sampler2D texture_specular1;
 uniform sampler2D texture_normal1;
 
+uniform vec4 v4_color;
+
 void main()
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+    FragColor = texture(texture_diffuse1, TexCoords) * v4_color;
 }
--- game/unicket/level4.cpp
+++ game/unicket/level4.cpp
@@ -71,6 +71,7 @@ void Level4::init_models()
 	auto* md3 = cube->get_component<Model>();
 	auto* trans3 = cube->get_component<Transform>();
 	md3->set_meshes(AssetManager::get_meshes("cube"));
+	md3->color.set(1, 0, 0, 1);
 	trans3->position.set(0.f, 5.f, -10.f);
 	trans3->scale.set(1.f, 1.f, 1.f);
 	register_object(cube);
@@ -80,6 +81,7 @@ void Level4::init_models()
 	auto* md4 = sphere->get_component<Model>();
 	auto* trans4 = sphere->get_component<Transform>();
 	md4->set_meshes(AssetManager::get_meshes("sphere"));
+	md4->color.set(0, 0, 1, 1);
 	trans4->position.set(0.f, -5.f, -10.f);
 	trans4->scale.set(1.f, 1.f, 1.f);
 	register_object(sphere);
